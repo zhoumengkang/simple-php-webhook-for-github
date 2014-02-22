8720747819e450c5f46d8f6a8b9e6c7789343b02
zhoumengkang <i@zhoumengkang>
2014-02-23 00:01:02 +0800

    增加一行注释

--- hook.php
+++ hook.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * description  : 仅仅只做更新（没有做日志记录，如果需要做日志记录，可以输出webhooks接口post传递过来的数据，更新信息都在里面）
+ * @author      : zhoumengkang
+ */
 error_reporting ( E_ALL );
 $dir = '/data/test/';//该目录为git检出目录
 $handle = popen('cd '.$dir.' && git pull 2>&1','r');
