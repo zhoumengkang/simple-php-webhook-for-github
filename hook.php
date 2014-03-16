d9bab440c8af76cfb57c500226529e2e268af399
周梦康 <i@zhoumengkang.com>
2014-03-16 09:01:38 +0800

    Update hook.php
    如果在$read中可以匹配到“error:”，就应该发送邮件到管理员的邮箱了！

--- hook.php
+++ hook.php
@@ -7,5 +7,6 @@ error_reporting ( E_ALL );
 $dir = '/data/test/';//该目录为git检出目录
 $handle = popen('cd '.$dir.' && git pull 2>&1','r');
 $read = stream_get_contents($handle);
+//TODO 如果在$read中可以匹配到“error:”，就应该发送邮件到管理员的邮箱了！
 printf($read);
 pclose($handle);
